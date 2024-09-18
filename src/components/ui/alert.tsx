bd1756b95ee70bba90eb122af8da466ab3e99299
Sleeyax <yourd3veloper@gmail.com>
2024-09-18 16:59:14 +0200

    feat: add success and warning colors

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -10,6 +10,8 @@ const alertVariants = cva(
         default: "bg-background text-foreground",
         destructive:
           "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
+        success: "border-success/50 text-success dark:border-success",
+        warning: "border-warning/50 text-warning dark:border-warning",
       },
     },
     defaultVariants: {
