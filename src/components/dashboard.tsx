970ae1de20a2c5183f6630de47b91fa816d94b1c
Sleeyax <yourd3veloper@gmail.com>
2024-09-26 20:45:55 +0200

    feat: disable search bar
    Disable for now. We can implement this later if needed.

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -123,16 +123,16 @@ export function Dashboard({ children }: PropsWithChildren) {
             </SheetContent>
           </Sheet>
           <div className="w-full flex-1">
-            <form>
+            {/*  <form>
               <div className="relative">
                 <Search className="absolute left-2.5 top-2.5 size-4 text-muted-foreground" />
                 <Input
                   type="search"
-                  placeholder="Search products..."
+                  placeholder="Search anything..."
                   className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
                 />
               </div>
-            </form>
+            </form> */}
           </div>
           <UserButton />
           <ThemeToggle />
